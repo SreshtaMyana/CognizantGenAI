09468281247f25b21175b8f071341185d434a19c
SreshtaMyana <sreshtamyana@gmail.com>
2025-05-21 23:08:16 -0400

    proj4 submit dis

--- proj4.py
+++ proj4.py
@@ -1,14 +1,32 @@
 '''
 Code for Project: Implement Your own Data Structures
 '''
-inventory = {}
-inventory['banana'] = (2, 2.50)
-inventory['pear'] = (4, 5.99)
-inventory['mango'] = (5, 8,99)
-inventory.remove('pear')
+inventory = {'grapes': (10, 4.0)}
+
+def add_item(k, val):
+    inventory[k] = val
+    print(f'Adding {k} to the inventory!')
+
+#adding items using a method
+add_item('banana', (2, 2.50))
+add_item('pear',(4, 5.99))
+add_item('mango', (5, 8,99))
+
+print('All items in the inventory: ')
+for i in inventory:
+    print(f'Item: {i}, Quantity: {inventory[i][0]}, Price: {inventory[i][1]}')
+
+
+#removing items
+del inventory['pear']
+print('removing \'pear\' from the inventory')
 
 #updating the price of banana
 li = list(inventory['banana'])
 li[1] = 1.99
 inventory['banana'] = li
-print('Updated inventory:')
+print('Updated inventory: ')
+for i in inventory:
+    print(f'Item: {i}, Quantity: {inventory[i][0]}, Price: {inventory[i][1]}')
+
+
